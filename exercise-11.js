1c5c32e1cf5f855754d33cc34e47d449f620719b
Arie Septian <ariesptn@gmail.com>
2018-11-19 20:51:12 +0700

    added exercise 10-13

---
+++ exercise-11.js
@@ -0,0 +1,14 @@
+function balikKata(kata) {
+    var s = '';
+    for (var i = kata.length; i > 0; i--) {
+        s = s + kata[i-1];
+    }
+    return s;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
---
+++ exercise-12.js
@@ -0,0 +1,15 @@
+function konversiMenit(menit) {
+    var h = parseInt(menit / 60);
+    var m = parseInt(menit % 60);
+    if (m < 10) {
+        m = '0' + m;
+    }
+    return h + ':' + m;
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
